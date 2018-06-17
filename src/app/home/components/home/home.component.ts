3f1c359b35d84e38533a18b20ff5519a17627abb
huannn1 <lix7@live.com>
2018-06-17 20:26:33 +0700

    Hello world

--- src/app/home/components/home/home.component.ts
+++ src/app/home/components/home/home.component.ts
@@ -6,10 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  isMenuActive = true;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleMenu() {
+    this.isMenuActive = !this.isMenuActive;
+  }
 }
